fc594bedd5cf95ea08767511e567809c141a9a77
Jalani Kanem <jalani@agnostiq.ai>
2022-02-11 01:12:28 -0500

    Added many comments and a few example lines to the custom executor template.

--- custom.py
+++ custom.py
@@ -2,18 +2,48 @@
 #
 # This file is a part of Covalent Cloud.
 
-"""Define a custom executor for the Covalent dispatcher."""
-
-from typing import Any
-
-from covalent._shared_files.util_classes import DispatchInfo
-from covalent._workflow.transport import TransportableObject
+# All executor plugins inherit from the BaseExecutor base class.
 from covalent.executor import BaseExecutor
 
-# Specify the plugin name as well as the short name given by this file's name
+# DispatchInfo objects are used to share info of a dispatched computation between different parts (electrons) of the workflow (lattice).
+from covalent._shared_files.util_classes import DispatchInfo
+
+# The function to be computed is in the form of a Covalent TransportableObject.
+# This import is not strictly necessary, as it is only used for type-hints.
+from covalent._workflow.transport import TransportableObject
+
+# The covalent logger moudle is a simple wrapper around the
+# standard Python logging module.
+from covalent._shared_files import logger
+app_log = logger.app_log
+log_stack_info = logger.log_stack_info
+
+# Results of operations take the form of io.StringIO objects.
+# These modules are used to capture those results.
+import io
+from contextlib import redirect_stderr, redirect_stdout
+
+# For type-hints
+from typing import Any
+
+# The plugin class name must be given by the executor_plugin_name attribute. In case this
+# module has more than one class defined, this lets Covalent know which is the executor class.
 executor_plugin_name = "CustomExecutor"
 
+
 class CustomExecutor(BaseExecutor):
+    def __init__(
+        # Inputs to an executor can be positional or keyword arguments.
+        self,
+        executor_input1: str,
+        executor_input2: int = 0,
+    ) -> None:
+        self.executor_input1 = executor_input1
+        self.executor_input2 = executor_input2
+
+        # Call the BaseExecutor initialization:
+        super().__init__()
+
     def execute(
         self,
         function: TransportableObject,
@@ -23,11 +53,55 @@ class CustomExecutor(BaseExecutor):
         node_id: int = -1,
     ) -> Any:
 
-        dispatch_info = DispatchInfo(dispatch_id)
+        """
+        Executes the input function and returns the result.
 
-        with self.get_dispatch_context(dispatch_info):
-            print(f"Execution args: {execution_args}")
+        Args:
+            function: The input (serialized) python function which will be executed and
+                whose result is ultimately returned by this function.
+            kwargs: Keyword arguments to be used by function.
+            execution_args: Executor-specific arguments.
+            dispatch_id: The unique identifier of the external lattice process which is
+                calling this function.
+            node_id: The node ID of this task in the bigger workflow graph.
 
-            # Execute the function here
+        Returns:
+            output: The result of the executed function.
+        """
 
-        return None
+        with self.get_dispatch_context(DispatchInfo(dispatch_id)), redirect_stdout(
+            io.StringIO()
+        ) as stdout, redirect_stderr(io.StringIO()) as stderr:
+
+            # Convert the function to be executed, which is in the form of
+            # a Covalent TransportableObject, to a 'standard' Python function:
+            fn = function.get_deserialized()
+            app_log.debug(type(fn))
+
+            # In this block is where operations specific to your custom executor
+            # can be defined. These operations could manipulate the function, the
+            # inputs/outputs, or anything that Python allows.
+            external_object = ExternalClass(3)
+            app_log.debug(external_object.multiplier)
+
+            # Here we simply execute the function on the local machine.
+            # But this could be sent to a more capable machine for the operation,
+            # or a different Python virtual environment, or more.
+            result = fn(**kwargs)
+
+            result = self.helper_function(result)
+
+            debug_message = f"Function '{fn.__name__}' was executed on node {node_id} with execution arguments {execution_args}"
+            app_log.debug(debug_message)
+
+        return (result, stdout.getvalue(), stderr.getvalue())
+
+    def helper_function(self, result):
+        return 2 * result
+
+
+# This class can be used in the custom executor, but will be ignored by the
+# plugin-loader, since it is not designated as the plugin class.
+class ExternalClass:
+    def __init__(self, multiplier: int):
+        self.multiplier = multiplier
