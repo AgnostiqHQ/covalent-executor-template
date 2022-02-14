5534708f1d07397a05a6b1926339c289483bc3b8
Jalani Kanem <jalani@agnostiq.ai>
2022-02-14 15:48:49 -0500

    Fixed typos and reworded some comments and docstrings.
    Added *args and **kwargs to inputs of the executor initialization.
    
    Relevant keyword args are passed along to BaseExecutor.
    
    Only the execution of the input function is within the
    stdout/stdouterr redirect block.

--- custom.py
+++ custom.py
@@ -5,14 +5,15 @@
 # All executor plugins inherit from the BaseExecutor base class.
 from covalent.executor import BaseExecutor
 
-# DispatchInfo objects are used to share info of a dispatched computation between different parts (electrons) of the workflow (lattice).
+# DispatchInfo objects are used to share info of a dispatched computation between different
+# tasks (electrons) of the workflow (lattice).
 from covalent._shared_files.util_classes import DispatchInfo
 
 # The function to be computed is in the form of a Covalent TransportableObject.
 # This import is not strictly necessary, as it is only used for type-hints.
 from covalent._workflow.transport import TransportableObject
 
-# The covalent logger moudle is a simple wrapper around the
+# The covalent logger module is a simple wrapper around the
 # standard Python logging module.
 from covalent._shared_files import logger
 app_log = logger.app_log
@@ -37,12 +38,31 @@ class CustomExecutor(BaseExecutor):
         self,
         executor_input1: str,
         executor_input2: int = 0,
+        *args,
+        **kwargs,
+
     ) -> None:
         self.executor_input1 = executor_input1
         self.executor_input2 = executor_input2
+        self.args = args
+        self.kwargs = kwargs
 
         # Call the BaseExecutor initialization:
-        super().__init__()
+        # There are a number of optional arguments to BaseExecutor
+        # that could be specfied as keyword inputs to CustomExecutor.
+        base_kwargs = {}
+        for key in self.kwargs:
+            if key in [
+                "log_stdout",
+                "log_stderr",
+                "conda_env",
+                "cache_dir",
+                "current_env_on_conda_fail",
+            ]:
+                base_kwargs[key] = self.kwargs[key]
+
+        super().__init__(**base_kwargs)
+    
 
     def execute(
         self,
@@ -63,36 +83,35 @@ class CustomExecutor(BaseExecutor):
             execution_args: Executor-specific arguments.
             dispatch_id: The unique identifier of the external lattice process which is
                 calling this function.
-            node_id: The node ID of this task in the bigger workflow graph.
+            node_id: The node ID of this task in the workflow graph.
 
         Returns:
             output: The result of the executed function.
         """
 
+        # Convert the function to be executed, which is in the form of
+        # a Covalent TransportableObject, to a 'standard' Python function:
+        fn = function.get_deserialized()
+        app_log.debug(type(fn))
+
+        # In this block is where operations specific to your custom executor
+        # can be defined. These operations could manipulate the function, the
+        # inputs/outputs, or anything that Python allows.
+        external_object = ExternalClass(3)
+        app_log.debug(external_object.multiplier)
+
+        # Here we simply execute the function on the local machine.
+        # But this could be sent to a more capable machine for the operation,
+        # or a different Python virtual environment, or more.
         with self.get_dispatch_context(DispatchInfo(dispatch_id)), redirect_stdout(
             io.StringIO()
         ) as stdout, redirect_stderr(io.StringIO()) as stderr:
-
-            # Convert the function to be executed, which is in the form of
-            # a Covalent TransportableObject, to a 'standard' Python function:
-            fn = function.get_deserialized()
-            app_log.debug(type(fn))
-
-            # In this block is where operations specific to your custom executor
-            # can be defined. These operations could manipulate the function, the
-            # inputs/outputs, or anything that Python allows.
-            external_object = ExternalClass(3)
-            app_log.debug(external_object.multiplier)
-
-            # Here we simply execute the function on the local machine.
-            # But this could be sent to a more capable machine for the operation,
-            # or a different Python virtual environment, or more.
             result = fn(**kwargs)
 
-            result = self.helper_function(result)
+        result = self.helper_function(result)
 
-            debug_message = f"Function '{fn.__name__}' was executed on node {node_id} with execution arguments {execution_args}"
-            app_log.debug(debug_message)
+        debug_message = f"Function '{fn.__name__}' was executed on node {node_id} with execution arguments {execution_args}"
+        app_log.debug(debug_message)
 
         return (result, stdout.getvalue(), stderr.getvalue())
 
