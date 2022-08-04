d862e25f74029fbf9c138f6ccdf8902f3a4e4577
Sankalp Sanand <sankalp@agnostiq.ai>
2022-08-04 11:07:52 -0400

    Update template with latest executor changes (#12)
    * PR creation
    
    * Made template changes and added async executor
    
    * Tests were not running before-fixed, added gitignore, fixed helper function
    
    * Updated changelog

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ with open("requirements.txt") as f:
 
 # Modify this to be the name of your plugin file. Here, "covalent_executor_template"
 # is the name of the directory the plugin is in. "custom" is name of the module.
-plugins_list = ["custom = covalent_executor_template.custom"]
+plugins_list = ["custom = covalent_executor_template.custom", "custom_async = covalent_executor_template.custom_async"]
 
 setup_info = {
     # Your plugin should use the naming convention 'covalent-abcdef-plugin'
