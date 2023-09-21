556fed8a036fd18e46220a27774f735606911bec
Will Cunningham <wjcunningham7@users.noreply.github.com>
2023-09-21 00:03:11 -0400

    updating license (#15)

--- setup.py
+++ setup.py
@@ -2,21 +2,17 @@
 #
 # This file is part of Covalent.
 #
-# Licensed under the GNU Affero General Public License 3.0 (the "License").
-# A copy of the License may be obtained with this software package or at
+# Licensed under the Apache License 2.0 (the "License"). A copy of the
+# License may be obtained with this software package or at
 #
-#      https://www.gnu.org/licenses/agpl-3.0.en.html
+#     https://www.apache.org/licenses/LICENSE-2.0
 #
-# Use of this file is prohibited except in compliance with the License. Any
-# modifications or derivative works of this file must retain this copyright
-# notice, and modified files must contain a notice indicating that they have
-# been altered from the originals.
-#
-# Covalent is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-# FITNESS FOR A PARTICULAR PURPOSE. See the License for more details.
-#
-# Relief from the License may be granted by purchasing a commercial license.
+# Use of this file is prohibited except in compliance with the License.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import site
 import sys
@@ -35,7 +31,10 @@ with open("requirements.txt") as f:
 
 # Modify this to be the name of your plugin file. Here, "covalent_executor_template"
 # is the name of the directory the plugin is in. "custom" is name of the module.
-plugins_list = ["custom = covalent_executor_template.custom", "custom_async = covalent_executor_template.custom_async"]
+plugins_list = [
+    "custom = covalent_executor_template.custom",
+    "custom_async = covalent_executor_template.custom_async",
+]
 
 setup_info = {
     # Your plugin should use the naming convention 'covalent-abcdef-plugin'
@@ -46,7 +45,7 @@ setup_info = {
     "maintainer": "Agnostiq",
     "url": "https://github.com/AgnostiqHQ/covalent-executor-template",
     "download_url": f"https://github.com/AgnostiqHQ/covalent-executor-template/archive/v{version}.tar.gz",
-    "license": "GNU Affero GPL v3.0",
+    "license": "Apache License 2.0",
     "author": "Agnostiq",
     "author_email": "support@agnostiq.ai",
     "description": "Covalent Custom Executor Plugin",
@@ -61,7 +60,7 @@ setup_info = {
         "Intended Audience :: Developers",
         "Intended Audience :: Education",
         "Intended Audience :: Science/Research",
-        "License :: Other/Proprietary License",
+        "License :: OSI Approved :: Apache Software License",
         "Natural Language :: English",
         "Operating System :: MacOS",
         "Operating System :: POSIX :: Linux",
